21bdba9710efb76cbaf50e7b60d58789f9bcfba0
dnr <n/a>
2018-03-05 14:41:49 -0800

    Fixed bug with numpy images having different dimensions in x and z. Also fixes potential bugs with unusual numpy memory orders

--- python/pyCudaImageWarp/cudaImageWarp.py
+++ python/pyCudaImageWarp/cudaImageWarp.py
@@ -8,15 +8,27 @@ import numpy as np
 import ctypes
 import pyCudaImageWarp
 
-
 """
 Arguments:
-        im -- An image volume, i.e. a 3D numpy array
-        A -- A [4x3] matrix defining the transformation
+        im -- An image volume, i.e. a 3D numpy array. Indexed in Fortran order,
+		e.g. im[x, y, z].
+        A -- A [4x3] matrix defining the transformation. A[0, :] applies to the
+		x-coordinates, A[1, :] the y-coordinates, A[2, :] the 
+		z-coordinates. See im for more details.
         interp -- The interpolation type. Supported values are either 
                 'linear' (default) or 'nearest'.
 """
 def cudaImageWarp(im, A, interp='linear'):
+
+	# Verify inputs
+	ndim = 3;
+	Ashape = (ndim, ndim + 1)
+	if len(im.shape) != ndim:
+		raise ValueError("im has shape %s, expected %d dimensions" % \
+			(im.shape, ndim))
+	if not np.equal(A.shape, Ashape).all():
+		raise ValueError("Expected A shape %s, received %s" % \
+			(Ashape, A.shape))
 
         # Mapping from interpolation strings to codes
         interpMap = {
@@ -24,24 +36,23 @@ def cudaImageWarp(im, A, interp='linear'):
                 'linear' : 1
         }
 
-        # Copy the image and extract its C interface
-        imFloat = np.copy(im).astype(np.float32)
-        AFloat = A.astype(np.float32)
-        imC = imFloat.ctypes
-        AC = AFloat.ctypes
+        # Convert the inputs to C float arrays
+	dtype = im.dtype
+        im = np.require(im, dtype='float32', requirements=['F', 'A', 'W', 'O'])
+        A = np.require(A, dtype='float32', requirements=['C', 'A'])
 
         # Warp the image
         ret = pyCudaImageWarp.warpfun(
-                imC.data_as(ctypes.POINTER(ctypes.c_float)),
+                im.ctypes.data_as(ctypes.POINTER(ctypes.c_float)),
                 ctypes.c_int(im.shape[0]), 
                 ctypes.c_int(im.shape[1]), 
                 ctypes.c_int(im.shape[2]), 
                 ctypes.c_int(interpMap[interp]),
-                AC.data_as(ctypes.POINTER(ctypes.c_float))
+                A.ctypes.data_as(ctypes.POINTER(ctypes.c_float))
         )
 
         if ret != 0:
                 raise ValueError(ret)
 
-        # Save the image as the original type
-        return imFloat.astype(im.dtype)
+        # Convert the output back to the original type
+        return im.astype(dtype)
